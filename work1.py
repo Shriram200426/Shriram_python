dfc32a35aab8834e7a1526c6db062a6ea8ae6b8f
shriram200426 <shriramnatrajan26@gmail.com>
2024-01-28 15:47:33 +0530

    first commit

---
+++ work1.py
@@ -0,0 +1,16 @@
+sum=0
+i=1
+while i<6:
+    sum=sum+i
+    i+=1
+print(sum) 
+
+
+for i in range(1,11,1):
+    print("11x",i,"=",11*i)
+
+
+fact=1
+for i in range(1,6):
+    fact=fact*i
+print(fact)
